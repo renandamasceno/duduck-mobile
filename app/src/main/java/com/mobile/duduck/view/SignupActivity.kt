18b501fbe1325cd73ef539f150a14dcc12f94a96
renandamasceno <jrferreirad@gmail.com>
2023-11-22 09:53:34 -0300

    feat: Implementing view signup

--- app/src/main/java/com/mobile/duduck/view/SignupActivity.kt
+++ app/src/main/java/com/mobile/duduck/view/SignupActivity.kt
@@ -5,29 +5,19 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Checkbox
-import androidx.compose.material3.CheckboxDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
@@ -37,14 +27,13 @@ import androidx.compose.ui.graphics.TileMode
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.mobile.duduck.R
-import com.mobile.duduck.ui.theme.DuduckTheme
 import com.mobile.duduck.view.components.TextFieldDefault
+import com.mobile.duduck.view.ui.theme.DuduckTheme
 
-class LoginActivity : ComponentActivity() {
+class SignupActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -53,7 +42,7 @@ class LoginActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = colorResource(id = R.color.black_background)
                 ) {
-                    LoginScreen()
+                    SignupScreen()
                 }
             }
         }
@@ -61,8 +50,7 @@ class LoginActivity : ComponentActivity() {
 }
 
 @Composable
-fun LoginScreen() {
-
+fun SignupScreen() {
     Box(
         modifier = Modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
@@ -84,7 +72,7 @@ fun LoginScreen() {
 
             TextFieldDefault(label = stringResource(R.string.e_mail_field_txt))
             TextFieldDefault(label = stringResource(R.string.password_field_txt))
-            CheckboxDefault()
+            TextFieldDefault(label = stringResource(R.string.repeat_password_field_txt))
 
             val gradientBrush = Brush.radialGradient(
                 colors = listOf(
@@ -106,88 +94,23 @@ fun LoginScreen() {
                 )
             ) {
                 Text(
-                    text = stringResource(R.string.enter_field_txt),
+                    text = stringResource(R.string.register_field_txt),
                     color = Color.White
                 )
             }
-            Box(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(top = 4.dp),
-                contentAlignment = Alignment.Center
-            ) {
-                Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                    Text(
-                        text = stringResource(R.string.or_field_txt),
-                        color = colorResource(id = R.color.gray_textfields_duduck)
-                    )
-                    Text(
-                        text = stringResource(R.string.signup_field_txt),
-                        color = colorResource(id = R.color.gray_textfields_duduck),
-                        textDecoration = TextDecoration.Underline,
-                        modifier = Modifier
-                            .padding(top = 8.dp)
-                            .clickable { TODO() }
-                    )
-                }
-            }
         }
-    }
-}
-
-@Composable
-fun CheckboxDefault() {
-    var checkedValue by remember {
-        mutableStateOf(false)
-    }
-    Row(
-        verticalAlignment = Alignment.CenterVertically,
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(horizontal = 8.dp)
-    ) {
-        Checkbox(
-            checked = checkedValue,
-            onCheckedChange = { newClick ->
-                checkedValue = newClick
-            },
-            modifier = Modifier
-                .background(
-                    color = Color.Transparent,
-                    shape = MaterialTheme.shapes.large
-                ),
-            colors = CheckboxDefaults.colors(
-                checkedColor = colorResource(id = R.color.orange_duduck)
-            )
-        )
-        Text(
-            text = stringResource(R.string.remember_me_field_txt),
-            color = colorResource(id = R.color.gray_textfields_duduck)
-        )
-        Spacer(modifier = Modifier.weight(1f))
-        Text(
-            text = stringResource(R.string.forgot_password_txt),
-            color = colorResource(id = R.color.gray_textfields_duduck),
-            textDecoration = TextDecoration.Underline,
-        )
     }
 }
 
 @Preview
 @Composable
-fun LoginActivityPreview() {
+fun SignupActivityPreview() {
     DuduckTheme {
         Surface(
             modifier = Modifier.fillMaxSize(),
             color = colorResource(id = R.color.black_background)
         ) {
-            Box(
-                modifier = Modifier.fillMaxSize(),
-                contentAlignment = Alignment.Center
-            ) {
-                LoginScreen()
-            }
+            SignupScreen()
         }
-
     }
 }
